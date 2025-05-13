cf81cf226aad5e6716ce0b3ac8c138eaae571166
vishal <vishal121002kumar@gmail.com>
2025-05-13 12:05:44 +0530

    updates cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+  app.enableCors({
+    origin: '*', // allow from anywhere
+    credentials: true, // allow cookies if needed (optional)
+  });
+
 
   await app.listen(process.env.PORT ?? 3000);
 }
