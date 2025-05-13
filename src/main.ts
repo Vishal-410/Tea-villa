7f55b090575f80219cd9ac37c2a6001aa38b3246
vishal <vishal121002kumar@gmail.com>
2025-05-13 12:36:29 +0530

    add binary target

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
- 
+  app.enableCors({
+    origin: '*', // allow from anywhere
+    credentials: true, // allow cookies if needed (optional)
+  });
+  // app.use(cors())
 
   await app.listen(process.env.PORT ?? 3000);
 }
